b517102dea526f9ec813eb0086f4a11d41fd8eea
Wyatt Colyn <wyattcolyn1248@gmail.com>
2022-02-25 15:23:33 -0700

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('https://wyattcolyn.github.io/notifymycrypto/client/sw.js', {
+  navigator.serviceWorker.register('/client/sw.js', {
     scope: '/',
   });
 }
