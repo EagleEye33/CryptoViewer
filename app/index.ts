a8c67d9f5e76153c65e6057c59c802565c252a61
Wyatt Colyn <wyattcolyn1248@gmail.com>
2022-03-04 11:29:11 -0700

    Adding API endpoint. testing network request

--- app/index.ts
+++ app/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 import database from './config/database.ts';
+import initializeRoutes from './routes';
 
 const app = express();
 const port = 8080; // default port to listen
@@ -11,6 +12,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
 
 database();
+initializeRoutes(app);
 
 // start the Express server
 app.listen(port, () => {
